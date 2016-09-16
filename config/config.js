4dfee83575e4a5cd2ff5903380e3304d1e2dc7df
delamorinierejh <delamorinierejh@gmail.com>
2016-09-16 10:57:43 +0100

    added mongolab uri

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db:   "mongodb://localhost/date-night",
+  db:   process.env.MONGOLAB_URI || "mongodb://localhost/date-night",
   secret: process.env.SECRET || "Hush hush hush",
   gmKey: process.env.GOOGLE_MAPS_KEY || "mapskey",
   zmKey: process.env.ZOMATO_API_KEY || "zomakey"
